553da65f3f98bbac8257dff95ca394bb8356b295
Your Name <your_email@whatever.com>
2025-12-03 17:12:10 +0300

    ADD mockito dependencies and agent. ADD tests implementation for the tested methods from the task to HorseTest.

--- src/test/java/HorseTest.java
+++ src/test/java/HorseTest.java
@@ -2,26 +2,28 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.MockedStatic;
 
+import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class HorseTest {
-    double speed = 1.0;
-    double acceleration = 1.0;
     String name = "test";
-
+    double speed = 1.0;
+    double distance = 1.0;
+    
     @Test
     @DisplayName("Test Horse constructor with null first parameter only Exception")
     void testHorseConstructorWithNullFirstParameterOnlyException() {
         assertThrows(IllegalArgumentException.class,
-            () -> new Horse(null,speed,acceleration));
+            () -> new Horse(null,speed, distance));
     }
 
     @Test
     @DisplayName("Test Horse constructor with null first parameter Exception with message")
     void testHorseConstructorWithNullFirstParameterExceptionWithMessage() {
         Exception exception = assertThrows(IllegalArgumentException.class,
-                () -> new Horse(null,speed,acceleration));
+                () -> new Horse(null,speed, distance));
         assertEquals("Name cannot be null.", exception.getMessage());
     }
 
@@ -30,7 +32,7 @@ class HorseTest {
     @DisplayName("Test Horse constructor with empty line or whitespace characters first parameter only Exception")
     void testHorseConstructorWithEmptyLineOrWhitespaceCharactersFirstParameterOnlyException(String whitespace) {
         assertThrows(IllegalArgumentException.class,
-                () -> new Horse(whitespace,speed,acceleration));
+                () -> new Horse(whitespace,speed, distance));
     }
 
     @ParameterizedTest
@@ -38,7 +40,7 @@ class HorseTest {
     @DisplayName("Test Horse constructor with empty line or whitespace characters first parameter Exception with message")
     void testHorseConstructorWithEmptyLineOrWhitespaceCharactersFirstParameterExceptionWithMessage(String whitespace) {
         Exception exception = assertThrows(IllegalArgumentException.class,
-                () -> new Horse(whitespace,speed,acceleration));
+                () -> new Horse(whitespace,speed, distance));
         assertEquals("Name cannot be blank.", exception.getMessage());
     }
 
@@ -46,14 +48,14 @@ class HorseTest {
     @DisplayName("Test Horse constructor with second parameter negative number only Exception")
     void testHorseConstructorWithSecondParameterNegativeNumberOnlyException() {
         assertThrows(IllegalArgumentException.class,
-                () -> new Horse(name,-1 * speed,acceleration));
+                () -> new Horse(name,-1 * speed, distance));
     }
 
     @Test
     @DisplayName("Test Horse constructor with second parameter negative number Exception with message")
     void testHorseConstructorWithSecondParameterNegativeNumberExceptionWithMessage() {
         Exception exception = assertThrows(IllegalArgumentException.class,
-                () -> new Horse(name, -1 * speed,acceleration));
+                () -> new Horse(name, -1 * speed, distance));
         assertEquals("Speed cannot be negative.", exception.getMessage());
     }
 
@@ -61,27 +63,55 @@ class HorseTest {
     @DisplayName("Test Horse constructor with third parameter negative number only Exception")
     void testHorseConstructorWithThirdParameterNegativeNumberOnlyException() {
         assertThrows(IllegalArgumentException.class,
-                () -> new Horse(name, speed,- 1 * acceleration));
+                () -> new Horse(name, speed,- 1 * distance));
     }
 
     @Test
     @DisplayName("Test Horse constructor with third parameter negative number Exception with message")
     void testHorseConstructorWithThirdParameterNegativeNumberExceptionWithMessage() {
         Exception exception = assertThrows(IllegalArgumentException.class,
-                () -> new Horse(name, speed, -1 * acceleration));
+                () -> new Horse(name, speed, -1 * distance));
         assertEquals("Distance cannot be negative.", exception.getMessage());
     }
 
     @Test
-    void getName() {
+    @DisplayName("Test get the name which is equal to the first parameter of the constructor ")
+    void testGetTheNameWhichIsEqualToTheFirstParameterOfTheConstructor() {
+        Horse horse = new Horse(name, speed, distance);
+        assertEquals(name, horse.getName());
     }
 
     @Test
-    void getSpeed() {
+    @DisplayName("Test get a number that is equal to the second parameter of the constructor")
+    void testGetANumberThatIsEqualToTheSecondParameterOfTheConstructor() {
+        Horse horse = new Horse(name, speed, distance);
+        assertEquals(speed, horse.getSpeed());
     }
 
     @Test
-    void getDistance() {
+    @DisplayName("Test get a number that is equal to the third parameter of the constructor")
+    void testGetANumberThatIsEqualToTheThirdParameterOfTheConstructor() {
+        Horse horse = new Horse(name, speed, distance);
+        assertEquals(distance, horse.getDistance());
+    }
+
+    @Test
+    @DisplayName("Test get zero in distance if two parameters are passed to the constructor ")
+    void testGetZeroInDistanceIfTwoParametersArePassedToTheConstructor() {
+        Horse horse = new Horse(name,speed);
+        assertEquals(0, horse.getDistance());
+    }
+
+    @Test
+    @DisplayName("Test to check that the method calls the getRandomDouble method internally with parameters 0.2 and 0.9")
+    void testToCheckThatTheMethodCallsTheGetRandomDoubleMethodInternallyWithParameters02And09() {
+        double expectedFirst = 0.2;
+        double expectedSecond = 0.9;
+        try (MockedStatic<Horse> mocked = mockStatic(Horse.class)) {
+            Horse horse = new Horse(name,speed,distance);
+            horse.move();
+            mocked.verify(() -> Horse.getRandomDouble(eq(expectedFirst),eq(expectedSecond)),times(1));
+        }
     }
 
     @Test
